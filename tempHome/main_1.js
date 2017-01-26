f5002e99869c8157308034a412934868d56f5053
Don Henton <donhenton@gmail.com>
2017-01-26 13:13:50 -0600

    adding d3 graph demo 2

--- tempHome/main_1.js
+++ tempHome/main_1.js
@@ -1,14 +1,7 @@
-var paramSettings =
-        {
-            'axisColor': '#00000'
-        }
-
-
-
 var data = [
-    {key: "Food Brands", value: 2},
-    {key: "Award Shows", value: .8},
-    {key: "Motherhood", value: 1.05}
+    {key: "Food Brands", value: 132},
+    {key: "Award Shows", value: 71},
+    {key: "Motherhood", value: 88} 
 ];
 var w = 800;
 var h = 450;
@@ -25,17 +18,12 @@ var params =
         {
             data: data,
             height: height,
-            width: width,
-            baseline: 1
+            width: width
 
 
         };
-function getColorForBar(i)
-{
-    // var ordinalColorScale = d3.scale.category20();
-    return '#ddd'
 
-}
+
 /* global d3 */
 function init()
 {
@@ -49,8 +37,42 @@ function init()
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
+    var controls = d3.select("body")
+            .append("div")
+            .classed('controls', true);
 
+    var sort_btn = controls.append("button")
+            .html("Sort data: ascending")
+            .attr("state", 0);
 
+    sort_btn.on("click", function () {
+        var self = d3.select(this);
+        var ascending = function (a, b) {
+            return a.value - b.value;
+        };
+        var descending = function (a, b) {
+            return b.value - a.value;
+        }
+
+        var state = +self.attr("state");
+        var txt = "Sort data: ";
+        if (state === 0) {
+            data.sort(ascending);
+            state = 1;
+            txt += "descending";
+        } else if (state === 1) {
+            data.sort(descending);
+            state = 0;
+            txt += "ascending";
+        }
+        self.attr("state", state);
+        self.html(txt);
+
+        plot.call(chart, params, false);
+
+    });
+
+    sort_btn.classed("button", true);
 
 
     plot.call(chart, params, true);
@@ -58,37 +80,58 @@ function init()
 
 function drawAxis(params, x, y, initialize)
 {
-    var xAxis = d3.svg.axis().tickSize(0)
+    var xAxis = d3.svg.axis()
             .scale(x)
             .orient("bottom");
-    var yAxis = d3.svg.axis().tickSize(0)
+    var yAxis = d3.svg.axis()
             .scale(y)
             .orient("left");
 
     if (initialize)
     {
 
+        var yGridlines = d3.svg.axis()
+                .scale(y)
+                .tickSize(-params.width, 0, 0)
+                .tickFormat("")
+                .orient("left");
 
-        //labels for the x axis        
+        this.append("g")
+                .call(yGridlines)
+                .classed("gridline", true)
+                .attr("transform", "translate(0,0)");
+
         this.append("g")
                 .classed("x axis", true)
                 .attr("transform", "translate(" + 0 + "," + params.height + ")")
                 .call(xAxis)
                 .selectAll("text")
                 .classed('x-axis-label', true)
-                .style("text-anchor", "center")
-                //.attr("dx", -8)
-                .attr("dy", 20)
-
+                .style("text-anchor", "end")
+                .attr("dx", -8)
+                .attr("dy", 8)
+                .attr("transform", "translate(0,0) rotate(-45)");
 
         this.append("g")
                 .classed("y axis", true)
                 .attr("transform", "translate(0,0)")
                 .call(yAxis);
 
+        this.select(".y.axis")
+                .append("text")
+                .attr("x", 0)
+                .attr("y", 0)
+                .style("text-anchor", "middle")
+                .attr("transform", "translate(-50," + params.height / 2 + ") rotate(-90)")
+                .text("Units sold");
 
-        this.selectAll("g.y.axis text").attr("visibility", "hidden");
-
+        this.select(".x.axis")
+                .append("text")
+                .attr("x", 0)
+                .attr("y", 0)
+                .style("text-anchor", "middle")
+                .attr("transform", "translate(" + params.width / 2 + ",80)")
+                .text("Donut type");
 
     }//initial
     else
@@ -101,7 +144,7 @@ function drawAxis(params, x, y, initialize)
                 .style("text-anchor", "end")
                 .attr("dx", -8)
                 .attr("dy", 8)
-
+                .attr("transform", "translate(0,0) rotate(-45)");
 
         this.selectAll("g.y.axis")
                 .transition()
@@ -119,7 +162,7 @@ function drawAxis(params, x, y, initialize)
 
 function plot(params, initialize) {
 
-
+    var ordinalColorScale = d3.scale.category20();
 
     var x = d3.scale.ordinal()
             .domain(params.data.map(function (entry) {
@@ -143,54 +186,37 @@ function plot(params, initialize) {
             .append("rect")
             .classed("bar", true)
             .on("mouseover", function (d, i) {
-
+                d3.select(this).style("fill","yellow");
             })
             .on("mousemove", function (d, i) {
-
+                
             })
             .on("mouseout", function (d, i) {
-
+                d3.select(this).style("fill",ordinalColorScale(i));
             })
-    var baseLineGroup = this.append('g');
-
-    //the base line
-    baseLineGroup.append("line")
-            .classed("baseline-line", true)
-            .style("stroke", "black")
-            .style("stroke-dasharray", "10, 5")
-            .attr("x1", 0)
-            .attr("y1", y(params.baseline))
-            .attr("x2", params.width)
-            .attr("y2", y(params.baseline));
-
-    baseLineGroup.append("text")
-            .classed("baseline-label", true)
-           // .attr("text-anchor", "right")
-            .attr('font-weight','bolder')
-            //.attr('font-size','20px')
-            .attr("transform", "translate("+(params.width*.9)+","+(y(params.baseline) - 7) + ")")
-            .text("Baseline 1.0");
-
+    this.selectAll(".bar-label")
+            .data(params.data)
+            .enter()
+            .append("text")
+            .classed("bar-label", true);
 
     //update
     this.selectAll(".bar")
-
+            .transition().duration(500).ease("bounce")
             .attr("x", function (d, i) {
-
-                return  x(d.key) + x.rangeBand() * .125;
-
+                return  x(d.key);
             })
             .attr("y", function (d, i) {
-                return  y(d.value) - 1;
+                return  y(d.value);
             })
             .attr("height", function (d, i) {
                 return params.height - y(d.value);
             })
             .attr("width", function (d) {
-                return  x.rangeBand() * .75;
+                return  x.rangeBand();
             })
             .style("fill", function (d, i) {
-                return getColorForBar(i);
+                return ordinalColorScale(i);
             });
 
 
